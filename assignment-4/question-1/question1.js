3734eee629d465bb3cf79521f2226e9da505aa43
Kibreab Ande Habte <43453230+Kibreab-109184@users.noreply.github.com>
2019-05-03 13:39:17 -0500

    Add files via upload

---
+++ assignment-4/question-1/question1.js
@@ -0,0 +1,29 @@
+
+const {Observable} = require('rxjs');
+const os = require('os');
+
+//system memory information
+let cpu = os.cpus();
+let numOfCores = cpu.length;
+let ramSize = os.totalmem()/(1024*1024*1024)   //convert to GB
+
+//required function
+let checkSystem = ()=>{
+    return Observable.create((observer)=>{
+        observer.next({
+            cpu, numOfCores, ramSize
+        });
+    });
+};
+
+//out-puting based on the conditions
+checkSystem().subscribe(data => {
+    console.log('Checking your system...')
+    if(data.numOfCores < 2){
+        console.log("Processor is not supported")
+    }else if(data.ramSize < 4){
+        console.log('This app needs at least 4 GB of RAM')
+    }else {
+        console.log('System is checked successfully')
+    }
+});
